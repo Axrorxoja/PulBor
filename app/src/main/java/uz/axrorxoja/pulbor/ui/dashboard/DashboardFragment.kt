6aa7959187620cf21badd00d707da46cd696fa90
Akhror Yodgorov <akhror_yodgorov@epam.com>
2022-08-07 15:50:12 +0500

    Task-5: Add Tab data layer
      - Rename classes and packages

--- app/src/main/java/uz/axrorxoja/pulbor/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/uz/axrorxoja/pulbor/ui/dashboard/DashboardFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import uz.axrorxoja.pulbor.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
